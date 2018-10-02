1ecf75bf0beb898e3cbea16f0259f4927b2e3482
Daniel Hertzberg <burnyd@gmail.com>
2018-10-02 12:56:34 -0400

    made changes to netbox python

--- pynetboxtesting.py
+++ pynetboxtesting.py
@@ -1,25 +1,21 @@
 import pynetbox
+import netaddr
 
-import pynetbox
+ip = netaddr.ip.IPNetwork('192.168.0.0/20')
+subnets = list(ip.subnet(24))
+
 nb = pynetbox.api(
     'http://localhost:32768',
     token='0123456789abcdef0123456789abcdef01234567'
 )
 
+testdict = {u'vrf': None, u'family': 4}
+
 def create_networks():
-    for i in networks
-    nb.ipam.prefixes.create([{u'prefix': u'10.1.1.44/30', u'vrf': None, u'family': 4}])
+    for i in subnets:
+       print 'Add the %s networks to Netbox' % (i)
+       testdict.update({'prefix': str(i) })       
+       nb.ipam.prefixes.create(testdict)
+       #[{u'prefix': u'10.1.1.44/30', u'vrf': None, u'family': 4}] This is an example of an API call to add the 10.1.1.44/30 network
 
-
-
-#prefix.available_prefixes.list()
-#prefix = nb.ipam.prefixes.all()
-
-
-#curl -H "Accept: application/json; indent=4" http://localhost:8000/api/ipam/ip-addresses/350/ | jq '.
-#curl -H "Accept: application/json; indent=4" http://localhost/api/dcim/devices/29/
-
-
-#import pynetbox
-#print(nb.virtualization.virtual_machines.all())
-#prefix = nb.ipam.prefixes.create([{u'prefix': u'10.1.1.44/30', u'vrf': None, u'family': 4}])
+create_networks()
