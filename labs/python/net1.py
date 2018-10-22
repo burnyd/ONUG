c9ce0e297b72acbd73195392f2123a7ecdb19f03
Daniel Hertzberg <burnyd@gmail.com>
2018-10-21 20:30:38 -0400

    chnged some lab stuff

--- labs/python/net1.py
+++ labs/python/net1.py
@@ -1,12 +1,10 @@
 import netaddr
 
-Loopback1 = ‘10.1.1.255’
-Loopback2 = ‘10.1.1.555’
+Loopback1 = "10.1.1.255"
+Loopback2 = "10.1.1.555"
 
-netaddr.IPnetwork(loopback1)
+netaddr.IPNetwork(Loopback1)
 
-netaddr.IPnetwork(loopback2)
-
-svi = ‘10.1.2.0’
+svi = "10.1.2.0"
 
 netaddr.IPNetwork(svi)
